a122b0e0d33bac728e62c4860373363fb38a114e
dan <danya.belyaev@gmail.com>
2023-10-21 15:55:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/givegift/ui/account/AccountViewModel.java
@@ -0,0 +1,18 @@
+package com.example.givegift.ui.account;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class AccountViewModel extends ViewModel {
+    private final MutableLiveData<String> mText;
+
+    public AccountViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
