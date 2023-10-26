717e3b4d7a054b850af27a4a95ba20835a8fc6b9
dan <danya.belyaev@gmail.com>
2023-10-26 23:49:53 +0300

    Add class and resources

---
+++ app/src/main/java/com/example/givegiftdesign/GiftBlock.java
@@ -0,0 +1,42 @@
+package com.example.givegiftdesign;
+
+import java.util.Objects;
+
+public class GiftBlock {
+    private String imageUrl;
+    private String description;
+    private String giftUrl;
+
+    public GiftBlock(String imageUrl, String description, String giftUrl) {
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.giftUrl = giftUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getGiftUrl() {
+        return giftUrl;
+    }
+
+    // А я переопределил))
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GiftBlock giftBlock = (GiftBlock) o;
+        return imageUrl.equals(giftBlock.imageUrl) && description.equals(giftBlock.description) && giftUrl.equals(giftBlock.giftUrl);
+    }
+
+    // А я переопределил))
+    @Override
+    public int hashCode() {
+        return Objects.hash(imageUrl, description, giftUrl);
+    }
+}
