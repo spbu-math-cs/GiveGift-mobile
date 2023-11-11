ab1b8239312381fba5b4e4c991eb476c35ccb1ae
dan <danya.belyaev@gmail.com>
2023-11-11 05:38:22 +0300

    I want to sleep and i go to sleep

--- app/src/main/java/com/example/givegiftdesign/MainActivity.java
+++ app/src/main/java/com/example/givegiftdesign/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         );
     }
 
+    // Люблю попугов
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
