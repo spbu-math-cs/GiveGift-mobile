2f9ee610923e025aa25d5095abb776d017616618
Alex <durn_alex@mail.ru>
2023-12-24 18:41:08 +0300

    Mmm, salt

--- app/src/main/java/com/example/givegiftdesign/ProfileActivity.java
+++ app/src/main/java/com/example/givegiftdesign/ProfileActivity.java
@@ -1,3 +1,17 @@
+package com.example.givegiftdesign;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
+
 /*
 package com.example.givegiftdesign;
 
