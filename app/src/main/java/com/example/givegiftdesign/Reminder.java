7e26227cd92607a0fa59accf650acbe1bb039820
dan <danya.belyaev@gmail.com>
2023-12-26 07:43:31 +0300

    I'm ded inside. But i did it

--- app/src/main/java/com/example/givegiftdesign/Reminder.java
+++ app/src/main/java/com/example/givegiftdesign/Reminder.java
@@ -9,19 +9,45 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import java.util.Random;
+
 public class Reminder extends BroadcastReceiver {
 
     private static final String CHANNEL_ID = "1";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
-                .setSmallIcon(R.mipmap.logout)
-                .setContentTitle("Напоминалка")
-                .setContentText("Давно тебя не было в уличных гонках")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setAutoCancel(true);
+        Random random = new Random();
 
+        NotificationCompat.Builder builder = null;
+        
+        switch (random.nextInt(3)) {
+            case 0:
+                builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                        .setSmallIcon(R.mipmap.logout)
+                        .setContentTitle("Напоминалка")
+                        .setContentText("Давно тебя не было в уличных гонках")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setAutoCancel(true);
+                break;
+            case 1:
+                builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                        .setSmallIcon(R.mipmap.logout)
+                        .setContentTitle("Сова")
+                        .setContentText("Знаешь сову из duolingo? Всеки ей")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setAutoCancel(true);
+                break;
+            case 2:
+                builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                        .setSmallIcon(R.mipmap.logout)
+                        .setContentTitle("Напоминалка")
+                        .setContentText("Скоро новый год. Ты ведь не забыл подготовить подарки?")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setAutoCancel(true);
+                break;
+        }
+        
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
         if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
             // here to request the missing permissions, and then overriding
